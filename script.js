9d30dcc4fe56ca27b592a165c63a049be531421e
Nuribe92 <nestoruribe92@hotmail.com>
2023-04-08 17:11:23 -0500

    animation fixed

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ document.addEventListener("DOMContentLoaded", function(){
             console.log(entry)
             if(entry.isIntersecting){
                 entry.target.classList.add('show');
-            }else{
-                entry.target.classList.remove('show');
             }
+            // }else{
+            //     entry.target.classList.remove('show');
+            // }
         });
     });
     
